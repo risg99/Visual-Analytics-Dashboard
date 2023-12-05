42f212bf955d2689f9a2331cd3775a14d6ca59f4
risg99 <rishikagupta1999@gmail.com>
2023-12-05 17:19:01 +0100

    add 4yr data, legend and better css

--- js/script.js
+++ js/script.js
@@ -26,6 +26,7 @@ function convertToInt(node) {
 
 function updateChartForYear(selectedYear) {
     document.getElementById('myDiv').innerHTML = '';
+    document.getElementById('myLegend').innerHTML = '';
 
     var fileName = `./data/data_${selectedYear}.json`;
 
@@ -42,9 +43,9 @@ function updateChartForYear(selectedYear) {
         convertToInt(data);
 
         // Define height, width and margin
-        var width = 600;
-        var height = 600;
-        var margin = { top: 20, right: 20, bottom: 20, left: 20 };
+        var margin = { top: 10, right: 10, bottom: 10, left: 10 };
+        var width = 600 - margin.left - margin.right;
+        var height = 600 - margin.top - margin.bottom;
 
         // Create svg element with the chosen height and width
         var svg = d3.select("#myDiv")
@@ -110,10 +111,92 @@ function updateChartForYear(selectedYear) {
                 return i === 0 ? "bold" : "normal";
             })
             .attr("dy", "0.35em")
-            .text(function (d, i) { if (i === 0) { return `For the year: ${selectedYear}`; } else { return d.data.name; } })
             .attr("text-anchor", "middle")
             .attr("font-size", "12px")
-            .attr("fill", "#000");
+            .attr("fill", "#000")
+            // .text(function (d, i) {
+            //     if (i === 0) {
+            //         return `For the year: ${selectedYear}`;
+            //     } else if (d.data.name === "Didnot Seek Help") {
+            //         return d.data.name.split("\n");
+            //     } else { return d.data.name; }
+            // });
+            .each(function (d, i) {
+                var labelLines = [];
+                if (i === 0) {
+                    labelLines.push(`For the year: ${selectedYear}`);
+                } else if (d.data.name === "Didnot Seek Help") {
+                    labelLines = d.data.name.split(" ");
+                } else {
+                    labelLines.push(d.data.name);
+                }
+
+                // Calculate total height for multiline text
+                var totalHeight = labelLines.length * 1.2;
+
+                // Add tspan elements for multiline text
+                for (var j = 0; j < labelLines.length; j++) {
+                    d3.select(this).append("tspan")
+                        .attr("x", 0)
+                        .attr("y", -totalHeight / 2 + j * 1.2 + "em") // Align text to center vertically
+                        .attr("text-anchor", "middle") // Align text to center horizontally
+                        .text(labelLines[j]);
+                }
+            });
+
+        // Define legend data
+        var legendData = [
+            { label: 'Male', color: '#a64d79' },
+            { label: 'Male, Perceived Normal', color: '#c27ba0' },
+            { label: 'Male, Not Perceived Normal', color: '#c27ba0' },
+            { label: 'Male, Perceived Normal, Didnot Seek Help', color: '#e36899' },
+            { label: 'Male, Not Perceived Normal, Didnot Seek Help', color: '#e36899' },
+            { label: 'Male, Perceived Normal, Sought Help', color: '#ead1dc' },
+            { label: 'Male, Not Perceived Normal, Sought Help', color: '#ead1dc' },
+
+            { label: 'Female', color: '#45818e' },
+            { label: 'Female, Perceived Normal', color: '#76a5af' },
+            { label: 'Female, Not Perceived Normal', color: '#76a5af' },
+            { label: 'Female, Perceived Normal, Didnot Seek Help', color: '#4ee6e6' },
+            { label: 'Female, Not Perceived Normal, Didnot Seek Help', color: '#4ee6e6' },
+            { label: 'Female, Perceived Normal, Sought Help', color: '#d0e0e3' },
+            { label: 'Female, Not Perceived Normal, Sought Help', color: '#d0e0e3' },
+        ];
+
+        // Calculate legend dimensions and positioning
+        var legendWidth = 1000;
+        var legendHeight = legendData.length * 25;
+        var legendX = 10; // X position of the legend
+        var legendY = 10; // Y position of the legend
+
+        // Create legend SVG container
+        var legendSvg = d3.select("#myLegend")
+            .append("svg")
+            .attr("width", legendWidth)
+            .attr("height", legendHeight)
+            .append("g")
+            .attr("transform", "translate(" + legendX + "," + legendY + ")");
+
+        // Add legend items
+        var legendItems = legendSvg.selectAll('.legend-item')
+            .data(legendData)
+            .enter().append('g')
+            .attr('class', 'legend-item')
+            .attr('transform', function (d, i) {
+                return 'translate(0,' + (i * 20) + ')';
+            });
+
+        legendItems.append('rect')
+            .attr('width', 12)
+            .attr('height', 12)
+            .attr('fill', function (d) { return d.color; });
+
+        legendItems.append('text')
+            .attr('x', 20)
+            .attr('y', 10)
+            .text(function (d) { return d.label; })
+            .attr('font-size', '12px')
+            .attr('fill', '#000');
 
     }).catch(function (error) {
         console.log(error);
